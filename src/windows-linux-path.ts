09c4758e6bae3667c22cc3b776561e52d680b3f3
Showtc <show@ikmail.com>
2025-12-29 19:12:54 +0100

    feat: check if path valid

--- src/windows-linux-path.ts
+++ src/windows-linux-path.ts
@@ -1,4 +1,4 @@
-import { LaunchProps, showHUD, Clipboard } from "@raycast/api";
+import { LaunchProps, Clipboard, showToast, Toast } from "@raycast/api";
 
 interface PathArgument {
   path: string;
@@ -6,14 +6,27 @@ interface PathArgument {
 
 export default async function main(props: LaunchProps<{ arguments: PathArgument }>) {
   let { path } = props.arguments;
+  
+  // Condition to test if a path is a Windows type path
+  if (/^[A-Za-z]:\\/.test(path) != true) {
+    await showToast({
+      style: Toast.Style.Failure,
+      title: "Path not valid",
+    });
+    return;
+  }
 
   // Replace backslashes to slashes
-  const regex = /\\/gi;
-  path = path.replace(regex, "/");
+  const REGEX = /\\/gi;
+  path = path.replace(REGEX, "/");
 
   // Add a slash at the beginning, then lower case the first character, and remove the ":"
   path = "/" + path.substring(0, 1).toLowerCase() + path.substring(2);
 
   await Clipboard.copy(path);
-  await showHUD("Copied path to clipboard");
+  await showToast({
+    style: Toast.Style.Success,
+    title: "Copied to clipboard",
+    message: path,
+  });
 }
